13297a3b603f9d1446b402f357f7c26da32cd705
suqun zhang <susizhang2017@gmail.com>
2022-11-14 15:58:00 +0100

    Adding footer section

--- crossover-group2/src/App.js
+++ crossover-group2/src/App.js
@@ -4,14 +4,18 @@ import { Awards } from "./components/home/awards/Awards";
 import { Location } from "./components/home/location/Location";
 import { Agents } from "./components/home/agents/Agents";
 import { Empty } from "./components/Empty";
+import Price from "./components/home/price/Price";
+import Footer from "./components/common/Footer";
 
 function App() {
   return (
     <div className="App">
+      <Price />
       <Awards />
       <Location />
       <Agents />
       <Empty />
+      <Footer />
     </div>
   );
 }
